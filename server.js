40247501842dd7f28d8e7d4ef94bc8a21f7e5e81
nerdynomad <nerdynomadx@gmail.com>
2020-11-04 20:56:11 +0530

    Initial Setup

---
+++ server.js
@@ -0,0 +1,27 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const expressLayouts = require('express-ejs-layouts')
+const indexRouter= require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+app.use('/', indexRouter)
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to mongoose'))
+
+
+app.listen(process.env.port || 8000, () => { 
+    console.log(`Server running at port 8000`)
+})
